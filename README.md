d16812c649dbca465dae41cf10016eb96bdc3ad8
salmonrizoev <salmonrizoev7@gmail.com>
2025-04-12 16:11:47 +0000

    new version

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# terraform-february-2025
+# craete main.tf and provide following
+
+```hcl
+module "vpc1" {
+  source  = "SalmonRizoev/vpc1/aws"
+  version = "0.2.0"
+
+  vpc_cidar = "172.31.0.0/16"
+  subnet1_cidar = "172.31.1.0/24"
+  subnet2_cidar = "172.31.2.0/24"
+  environment = "stage"
+} 
+```
+ 
--- main.tf
+++ main.tf
@@ -15,6 +15,17 @@ resource "aws_subnet" "mai1" {
   }
 }
 
+resource "aws_subnet" "mai2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet2_cidar
+
+  tags = {
+    Name = "${var.environment}-subnet2"
+  }
+}
+
+
 variable vpc_cidar {}
 variable subnet1_cidar {}
+variable subnet2_cidar {}
 variable environment {} 
